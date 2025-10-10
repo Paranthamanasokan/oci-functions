82e4fd8c8ba45c5d283266ad8dba6eede08ecce0
Paranthamanasokan <paranthaman.asokan@excelenciaconsulting.com>
2025-10-10 18:29:35 +0530

    test_local.py

--- sentinel-forwarder/test_local.py
+++ sentinel-forwarder/test_local.py
@@ -1,1 +1,12 @@
+# Quick local test (no OCI) to verify import & event shaping only.
+# It won't post to Sentinel unless you set env vars.
+import os
+from io import BytesIO
+from src.func import handler
 
+# set envs for local test if you want to actually post
+# os.environ["WORKSPACE_ID"] = "<your_workspace_id>"
+# os.environ["SHARED_KEY"] = "<your_primary_key_base64>"
+
+resp = handler(None, BytesIO())
+print(resp.body().decode() if hasattr(resp, "body") else resp)
